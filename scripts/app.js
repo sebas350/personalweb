188cfb044c889058183ea7e334d3d01723190123
sebas350 <159815606+sebas350@users.noreply.github.com>
2024-07-16 01:26:43 -0300

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -3,3 +3,19 @@ import { Home } from './components/home.js';
 import { Resume } from './components/resume.js';
 import { Portfolio } from './components/portfolio.js';
 import { Footer } from './components/footer.js';
+import { Main } from './components/main.js';
+
+function init(){
+  const root = document.querySelector('#root');
+  const nav = Navbar();
+  const home = Home();
+  const main = Main();
+  const resume = Resume();
+  const portfolio = Portfolio();
+  nav.appendChild(home);
+  nav.appendChild(resume);
+  nav.appendChild(portfolio);
+  root.appendChild(nav);
+  root.appendChild(home);
+  root.appendChild(footer);
+};
