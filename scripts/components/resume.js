01c2cc7ac6e0f46146ae03c74b87cb57ba888016
sebas350 <159815606+sebas350@users.noreply.github.com>
2024-07-28 22:59:10 -0300

    Update resume.js

--- scripts/components/resume.js
+++ scripts/components/resume.js
@@ -3,12 +3,19 @@ export function Resume() {
     const div = document.createElement('div');
     const h1 = document.createElement('h1');
     const p = document.createElement('p');
+    const circle = document.createElement('div');
+
+    circle.style.width = "100px";
+    circle.style.height = "100px";
+    circle.style.borderRadius = "50%";
+    circle.style.backgroundColor = 'blue';
 
     h1.textContent = 'Resume';
     p.textContent = '[Your Resume Information]';
 
     div.appendChild(h1);
     div.appendChild(p);
+    div.appendChild(circle);
 
     div.style.color = 'blue';
     div.style.border = '2px solid blue';
