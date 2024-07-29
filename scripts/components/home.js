e2649b699912fcf977bc4459bbb47ab756dfee7f
sebas350 <profesebastian8@gmail.com>
2024-07-29 04:22:28 -0300

    update

--- scripts/components/home.js
+++ scripts/components/home.js
@@ -5,24 +5,32 @@ export function Home() {
     const p = document.createElement('p');
     const circle = document.createElement('div');
 
-    circle.style.width = "100px";
-    circle.style.height = "100px";
+    circle.style.width = "300px";
+    circle.style.height = "300px";
     circle.style.borderRadius = "50%";
-    circle.style.backgroundColor = 'blue';
+    circle.style.backgroundImage= "url('img/professional-1.jpg')";
 
     h1.textContent = 'Welcome to My Personal Website';
     p.textContent = 'Hi, I\'m [Your Name], a Fullstack Developer.';
+    
+    h1.style.border = '2px solid blue';
+    p.style.border = '2px solid blue';
+    circle.style.border = '2px solid blue';
 
     div.appendChild(h1);
     div.appendChild(p);
     div.appendChild(circle);
+    
 
     div.style.color = 'blue';
+    div.style.height = '80vh';
     div.style.border = '2px solid blue';
     div.style.display = 'flex';
-    div.style.justifyContent = 'center';
-    div.style.alignItems = 'center';
     div.style.flexDirection = 'column';
+    div.style.justifyContent = 'space-around';
+    div.style.alignItems = 'center';
+    //div.style.padding = '0';
+    //div.style.margin = '0';
     container.appendChild(div);
     return container;
 }
