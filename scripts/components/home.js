81fe8a653e881c9a9afd94edcc16526c866a0d14
sebas350 <159815606+sebas350@users.noreply.github.com>
2024-07-31 20:53:30 -0300

    Update home.js

--- scripts/components/home.js
+++ scripts/components/home.js
@@ -43,7 +43,7 @@ export function Home() {
 const h1 = sectionMe.querySelector('h1');
     
     h1.style.color = 'blue';
-    //h1.style.textAlign = 'center';
+    h1.style.textAlign = 'justify';
     //p.style.border = '2px solid blue';
     //p.style.color = 'rgb(173,()216,230)';
     //h1.style.color = '#dd8e6';
