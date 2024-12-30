e6a381318b531225336392f3ab844d92774bea81
sebas350 <profesebastian8@gmail.com>
2024-12-30 14:51:08 -0300

    update cert

--- scripts/components/certificates.js
+++ scripts/components/certificates.js
@@ -12,6 +12,10 @@ export function Certificates
           cert1:{
               title:'Fullstack web developer',
               subtitle:'Fullstack Python Web Developer and Soft Skills',
+          },
+          cert2:{
+              title:'Fullstack web developer',
+              subtitle:'Fullstack Python Web Developer and Soft Skills',
           },    
       },
     },
@@ -22,6 +26,10 @@ export function Certificates
          cert1:{
               title:'Desarrollador Web fullstack',
               subtitle:'Desarrollador Web Fullstack Python y Habilidades Blandas',
+          },
+          cert2:{
+              title:'Desarrollador Web fullstack',
+              subtitle:'Desarrollador Web Fullstack Python y Habilidades Blandas',
           }, 
       }, 
     },
@@ -30,35 +38,38 @@ export function Certificates
   // Traducciones dinámicas
   const { title, subtitle, certificates } = translations[language];
 
-  const container = document.createElement('div');
-  const cert1 = document.createElement('div');
-  cert1.style.backgroundImage= "url('https://raw.githubusercontent.com/sebas350/personalweb/main/scripts/components/img/cert1.png')";
-  
-    cert1.style.width = "100%";
-    cert1.style.height = "500px";
-    cert1.style.backgroundSize = 'cover';
-    cert1.style.backgroundPosition = 'center';
+  const container = document.createElement('div'); 
   
   container.innerHTML = `
     <h1>${title}</h1>
     <p>${subtitle}</p>
+    
+    <p>${certificates.cert1.title}</p>
+    <p>${certificates.cert1.subtitle}</p>
+    
+    <img src="https://raw.githubusercontent.com/sebas350/personalweb/main/scripts/components/img/cert1.png" style="width:100%; margin:20px 0px;"  alt="">
+  
+    <p>${certificates.cert2.title}</p>
+    <p>${certificates.cert2.subtitle}</p>
+    
+    <img src="https://raw.githubusercontent.com/sebas350/personalweb/main/scripts/components/img/cert2.png" style="width:100%; margin:20px 0px;"  alt="">
+  
   `;
   
-  cert1.innerHTML = `
-  <p>${certificates.cert1.title}</p>
-  <p>${certificates.cert1.subtitle}</p> `;
+  const imgs = container.querySelectorAll('img');
   
-  container.appendChild(cert1);
 
-  //container.style.padding = '5%';
+  imgs.forEach(img => {
 
-  // Aplicar efecto de entrada con IntersectionObserver
-  const cards = container.querySelectorAll('div');
+    img.style.opacity = '0';
+    img.style.transform = 'translateX(-10%)';
 
-  cards.forEach((el) => {
-    el.style.opacity = '0';
-    el.style.transition = 'opacity 1500ms';
-  });
+    img.style.transition = 'transform 1000ms, opacity 1000ms';
+
+    img.style.borderRadius = '10px';
+    img.style.boxShadow = '2px 2px #E0E0E0';
+
+});
 
   const observer = new IntersectionObserver(
     (entries) => {
@@ -73,7 +84,7 @@ export function Certificates
     { threshold: 0.3 }
   );
 
-  cards.forEach((el) => observer.observe(el));
+  imgs.forEach((el) => observer.observe(el));
 
   return container;
 }
